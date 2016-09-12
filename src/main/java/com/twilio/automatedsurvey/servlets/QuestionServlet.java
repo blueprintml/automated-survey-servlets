93bf92d76f70e6de47c8f3356dc5d0d5c7b84d51
mosampaio <mosampaio@gmail.com>
2016-09-12 10:09:45 -0300

    Introduces twilio next-gen lib

--- src/main/java/com/twilio/automatedsurvey/servlets/QuestionServlet.java
+++ src/main/java/com/twilio/automatedsurvey/servlets/QuestionServlet.java
@@ -6,7 +6,10 @@ import com.twilio.automatedsurvey.servlets.twimlquestions.AbstractTwiMLQuestionF
 import com.twilio.automatedsurvey.survey.Question;
 import com.twilio.automatedsurvey.survey.Survey;
 import com.twilio.automatedsurvey.survey.SurveyRepository;
-import com.twilio.sdk.verbs.TwiMLResponse;
+import com.twilio.twiml.TwiML;
+import com.twilio.twiml.TwiMLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +20,9 @@ import java.util.Optional;
 
 @Singleton
 public class QuestionServlet extends HttpServlet {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(QuestionServlet.class);
+
     private SurveyRepository surveyRepository;
     private ResponseWriter responseWriter;
 
@@ -26,32 +32,39 @@ public class QuestionServlet extends HttpServlet {
         this.responseWriter = responseWriter;
     }
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Long surveyId = Long.parseLong(request.getParameter("survey"));
-        String questionId = request.getParameter("question");
+        try {
+            Long surveyId = Long.parseLong(request.getParameter("survey"));
+            String questionId = request.getParameter("question");
 
 
-        Optional<Survey> survey = surveyRepository.find(surveyId);
-        Optional<Question> question;
+            Optional<Survey> survey = surveyRepository.find(surveyId);
+            Optional<Question> question;
 
-        if (questionId == null) {
-            question = survey.flatMap((Survey s) -> s.getFirstQuestion());
-        } else {
-            Long parsedQuestionId = Long.parseLong(questionId);
-            question = survey.flatMap((Survey s) -> s.questionById(parsedQuestionId));
+            if (questionId == null) {
+                question = survey.flatMap((Survey s) -> s.getFirstQuestion());
+            } else {
+                Long parsedQuestionId = Long.parseLong(questionId);
+                question = survey.flatMap((Survey s) -> s.questionById(parsedQuestionId));
+            }
+
+            AbstractTwiMLQuestionFactory factory = AbstractTwiMLQuestionFactory.getInstance(request);
+
+            TwiML twiMLResponse = question.map((Question q) -> factory.build(surveyId, q))
+                    .orElseThrow(() -> new RuntimeException(String.format("Survey/question %s/%s not found",
+                            surveyId, questionId)));
+
+            HttpSession session = request.getSession(true);
+            session.setAttribute("lastSurvey", surveyId);
+            session.setAttribute("lastQuestion", question.get().getId());
+            String toXml = twiMLResponse.toXml();
+
+            LOGGER.info("response: {}", toXml);
+            responseWriter.writeIn(response, toXml);
+        } catch (TwiMLException e) {
+            e.printStackTrace();
         }
-
-        AbstractTwiMLQuestionFactory factory = AbstractTwiMLQuestionFactory.getInstance(request);
-
-        TwiMLResponse twiMLResponse = question.map((Question q) -> factory.build(surveyId, q))
-                .orElseThrow(() -> new RuntimeException(String.format("Survey/question %s/%s not found",
-                        surveyId, questionId)));
-
-        HttpSession session = request.getSession(true);
-        session.setAttribute("lastSurvey", surveyId);
-        session.setAttribute("lastQuestion", question.get().getId());
-
-        responseWriter.writeIn(response, twiMLResponse.toEscapedXML());
     }
 
 }
--- src/test/java/com/twilio/automatedsurvey/servlets/SurveyServletTest.java
+++ src/test/java/com/twilio/automatedsurvey/servlets/SurveyServletTest.java
@@ -3,20 +3,23 @@ package com.twilio.automatedsurvey.servlets;
 import com.twilio.automatedsurvey.survey.Question;
 import com.twilio.automatedsurvey.survey.Survey;
 import com.twilio.automatedsurvey.survey.SurveyRepository;
-import com.twilio.sdk.verbs.Redirect;
-import com.twilio.sdk.verbs.TwiMLException;
-import com.twilio.sdk.verbs.TwiMLResponse;
-import com.twilio.sdk.verbs.Verb;
+import com.twilio.twiml.TwiMLException;
 import org.junit.Test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Optional;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class SurveyServletTest {
 
@@ -25,12 +28,10 @@ public class SurveyServletTest {
         SurveyRepository surveyRepo = mock(SurveyRepository.class);
         when(surveyRepo.add(any(Survey.class))).thenReturn(new Survey("test"));
 
-        TwilioResponseFactory twilioResponseFactory = mock(TwilioResponseFactory.class);
-
         ResponseWriter responseWriter = mock(ResponseWriter.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
 
-        SurveyServlet surveyServlet = new SurveyServlet(surveyRepo, twilioResponseFactory, responseWriter);
+        SurveyServlet surveyServlet = new SurveyServlet(surveyRepo, responseWriter);
 
         surveyServlet.doGet(MockedHttpServletRequestFactory.getMockedRequestWithParameters(new HashMap<>()), response);
 
@@ -44,7 +45,7 @@ public class SurveyServletTest {
         when(surveyRepository.find(anyLong())).thenReturn(Optional.of(mockedSurvey));
         when(mockedSurvey.getNextQuestion(any(Question.class))).thenReturn(Optional.of(mock(Question.class)));
 
-        SurveyServlet surveyServlet = new SurveyServlet(surveyRepository, mock(TwilioResponseFactory.class),
+        SurveyServlet surveyServlet = new SurveyServlet(surveyRepository,
                 mock(ResponseWriter.class));
 
         HttpServletRequest request = MockedHttpServletRequestFactory.getMockedRequestWithParameters(new HashMap<String, String>() {{
